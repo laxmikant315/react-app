3dbc9e7c7effab93ce272a7e8086eb21e998e7f1
laxmikant315 <laxmikantphadke@gmail.com>
2019-03-18 23:35:41 +0530

    20180318

--- src/index.js
+++ src/index.js
@@ -3,8 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'typeface-roboto';
+import { createMuiTheme } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/styles";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = createMuiTheme({ typography: { useNextVariants: true } });
+
+
+ReactDOM.render(
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>,
+    document.querySelector("#root")
+  );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
